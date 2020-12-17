8a197614018b06b111ec0507116110dc1efe98b5
Sasha Fu <mariafu0801@gmail.com>
2020-12-17 11:34:06 +0800

    login title change , change React conpoment before

--- JavaScript/ifLogin.js
+++ JavaScript/ifLogin.js
@@ -1,10 +1,23 @@
 const notLoginState = document.querySelector(".notLoginState");
 const loginState = document.querySelector(".loginState");
 
-if (sessionStorage.getItem("user") === null) {
-  loginState.style.display = "none";
-  notLoginState.style.display = "flex";
-} else {
-  loginState.style.display = "flex";
-  notLoginState.style.display = "none";
-}
+// Firebase 監聽
+window.addEventListener("load", function () {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log("sign in");
+      console.log(user);
+      console.log(user.email);
+      loginState.style.display = "flex";
+      notLoginState.style.display = "none";
+    } else {
+      console.log("user is sign out");
+      console.log(user);
+      loginState.style.display = "none";
+      notLoginState.style.display = "flex";
+    }
+  });
+});
+
+const user = firebase.auth().currentUser;
+console.log(user);
--- JavaScript/member.js
+++ JavaScript/member.js
@@ -4,24 +4,20 @@ const content = document.querySelector(".content");
 const logoutBtn = document.querySelector("#logoutBtn");
 logoutBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  firebase.auth().signOut();
-  sessionStorage.removeItem("user");
-  console.log("User signed out!");
-  alert("登出成功");
-  location.href = "../../Html/homepage.html";
-});
 
-// Firebase 監聽
-// window.addEventListener("load", function () {
-//   firebase.auth().onAuthStateChanged((user) => {
-//     if (user) {
-//       console.log("sign in");
-//       console.log(user);
-//       console.log(user.email);
-//       content.innerHTML = `會員資料： ${user.email}`;
-//     } else {
-//       console.log("user is sign out");
-//       console.log(user);
-//     }
-//   });
-// });
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      // Sign-out successful.
+      sessionStorage.removeItem("user");
+      console.log("User signed out!");
+      alert("登出成功");
+      location.href = "../../Html/homepage.html";
+    })
+    .catch(function (error) {
+      // An error happened.
+      console.log("Signed out! Error");
+      alert("登出失敗");
+    });
+});
